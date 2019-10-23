b1f1c4165a155041034a78842573ea960117de8d
Zain Khalid <zak565@uowmail.edu.au>
2019-10-23 01:37:14 -0400

    Added some animation, wanted to add more when clicking on the dice but I'm unable to do so for now. Going to move on with the course

---
+++ lib/IntroAnimation.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+//class IntroAnimation extends StatefulWidget {
+//  IntroAnimation({Key key}) : super (key:key);
+//  @override
+//  State<StatefulWidget> createState() => _IntroAnimationState();
+//}
+//
+//class _IntroAnimationState extends {
+//}
--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,39 @@ class DicePage extends StatefulWidget {
   _DicePageState createState() => _DicePageState();
 }
 
-class _DicePageState extends State<DicePage> {
+class _DicePageState extends State<DicePage> with SingleTickerProviderStateMixin {
   int _left = 1;
   int _right = 1;
+  int introAnimationCount = 0;
+  Animation<double> introAnimation;
+  Animation<double> tapAnimation;
+  AnimationController controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this
+    );
+
+    introAnimation = Tween<double>(begin: 0.0, end: 200.0).animate(controller)
+                ..addStatusListener((status) {
+                  if (status == AnimationStatus.completed && introAnimationCount != 2) {
+                    controller.reverse();
+                  } else if (status == AnimationStatus.dismissed) {
+                    controller.forward();
+                    introAnimationCount++;
+                  }
+                  if (introAnimationCount == 2) {
+                    tapAnimation = CurvedAnimation (parent: controller, curve: Curves.bounceIn);
+                  }
+                })
+                ..addListener((){ setState(() {}); });
+
+    controller.forward();
+  }
 
   void indexUpdate() {
     _left = Random().nextInt(6) + 1;
@@ -42,10 +72,14 @@ class _DicePageState extends State<DicePage> {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice$_left.png'),
+              child: Image.asset('images/dice$_left.png',
+                height: introAnimation.value,
+                width: introAnimation.value
+              ),
               onPressed: () {
                 setState(() {
                   indexUpdate();
+                  controller.forward();
                 });
               }
             ),
@@ -53,10 +87,15 @@ class _DicePageState extends State<DicePage> {
           SizedBox (width: 10.0),
           Expanded (
             child: FlatButton(
-              child: Image.asset('images/dice$_right.png'),
+              child: Image.asset(
+                  'images/dice$_right.png',
+                  height: introAnimation.value,
+                  width: introAnimation.value
+              ),
               onPressed: () {
                 setState(() {
                   indexUpdate();
+                  controller.forward();
                 });
               },
             ),
@@ -65,4 +104,12 @@ class _DicePageState extends State<DicePage> {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+
 }
